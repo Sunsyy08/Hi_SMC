8ae91b8f8e84a6e129df2223c373459dba012284
Sunsyy08 <seonhyben864@gmail.com>
2025-08-30 23:58:46 +0900

    ADD SignInScreen

--- app/src/main/java/com/project/hismc/SignInScreen.kt
+++ app/src/main/java/com/project/hismc/SignInScreen.kt
@@ -1,9 +1,15 @@
 package com.project.hismc
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavController
 
 @Composable
 fun SignInScreen(navController: NavController) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
 }
