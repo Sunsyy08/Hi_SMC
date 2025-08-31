b706585c4622740021091d431b2a37d1915bbf29
Sunsyy08 <seonhyben864@gmail.com>
2025-08-31 11:55:18 +0900

    Update SignInScreen

--- app/src/main/java/com/project/hismc/SignInScreen.kt
+++ app/src/main/java/com/project/hismc/SignInScreen.kt
@@ -1,15 +1,125 @@
 package com.project.hismc
 
+import android.R.attr.text
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.CompositingStrategy
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.project.hismc.ui.theme.HismcTheme
 
 @Composable
 fun SignInScreen(navController: NavController) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ){
+        Box(
+            modifier = Modifier
+            .fillMaxSize()
+        ){
+            Canvas(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .statusBarsPadding()
+                    .graphicsLayer(
+                        compositingStrategy = CompositingStrategy.ModulateAlpha
+                    )
+            ) {
+                translate(left = -80f, top = -1000f, ) {
+                    drawCircle(Color(0xff40CEF2), radius = 350.dp.toPx())
+                    RoundedCornerShape(400.dp)
+                }
+            }
+            Image(
+                painter = painterResource(id = R.drawable.smc_ms),
+                contentDescription = "학교 마스코드",
+                modifier = Modifier
+                    .size(200.dp)
+                    .offset(x = (210.dp), y = (30.dp))
+            )
+            Text(
+                text = "Sign In",
+                fontSize = (60.sp),
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.White,
+                modifier = Modifier
+                    .offset(x = (30).dp, y = (260).dp),
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 500.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier
+                        .height(60.dp)
+                        .width(350.dp),
+                    value = email,
+                    onValueChange = {email = it},
+                    shape = RoundedCornerShape(20.dp),
+                    label = {Text("email")}
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                OutlinedTextField(
+                    modifier = Modifier
+                        .height(60.dp)
+                        .width(350.dp),
+                    value = password,
+                    onValueChange = {password = it},
+                    shape = RoundedCornerShape(20.dp),
+                    label = {Text("email")}
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview(){
+    HismcTheme { 
+        SignInScreen(navController = rememberNavController())
+    }
 }
