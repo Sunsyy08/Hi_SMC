21920e95547219c2d8e7a58903ab20182792553d
Sunsyy08 <seonhyben864@gmail.com>
2025-10-25 23:14:07 +0900

    Create UI

--- app/src/main/java/com/project/hismc/LostItemsScreen.kt
+++ app/src/main/java/com/project/hismc/LostItemsScreen.kt
@@ -1,9 +1,54 @@
 package com.project.hismc
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.project.hismc.ui.theme.HismcTheme
 
+//분실물 센터
 @Composable
 fun LostItemsScreen(navController: NavController) {
+    Scaffold() { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.White)
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "분실물 센터",
+                fontSize = 28.sp,
+                modifier = Modifier.padding(30.dp)
+            )
+            IconButton(
+                onClick = {}
+            ) {
+                
+            }
 
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LostItemsPreview() {
+    HismcTheme {
+        LostItemsScreen(navController = rememberNavController())
+    }
 }
